797d9b48687ed8e5f80895f1c87ee377a9783188
Leonard Jombo <158037209+lenardjombo@users.noreply.github.com>
2024-08-21 03:32:03 -0400

    Update IntgerToBinary.java

--- IntgerToBinary.java
+++ IntgerToBinary.java
@@ -1,11 +1,32 @@
 import java.util.Scanner;
-public class IntegerToBinary {
+
+public class Exercise19 {
     public static void main(String args[]) {
-        int num;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number to be converted to string: ");
-        num = scanner.nextInt();
-        String binaryString = Integer.toBinaryString(num);
-        System.out.print( binaryString);
+        // Declare variables to store decimal number, quotient, and an array for binary digits
+        int dec_num, quot, i = 1, j;
+        int bin_num[] = new int[100];
+        
+        // Create a Scanner object to read input from the user
+        Scanner scan = new Scanner(System.in);
+
+        // Prompt the user to input a decimal number
+        System.out.print("Input a Decimal Number: ");
+        dec_num = scan.nextInt();
+
+        // Initialize the quotient with the decimal number
+        quot = dec_num;
+
+        // Convert the decimal number to binary and store binary digits
+        while (quot != 0) {
+            bin_num[i++] = quot % 2;
+            quot = quot / 2;
+        }
+
+        // Display the binary representation of the decimal number
+        System.out.print("Binary number is: ");
+        for (j = i - 1; j > 0; j--) {
+            System.out.print(bin_num[j]);
+        }
+        System.out.print("\n");
     }
 }
