90301983707f77c5cf9f5d847fc0c7afdbc4fc64
Leonard Jombo <158037209+lenardjombo@users.noreply.github.com>
2024-08-22 01:04:40 -0400

    Create fizzbuzz.java

---
+++ fizzbuzz.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                answer.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                answer.add("Fizz");
+            } else if (i % 5 == 0) {
+                answer.add("Buzz");
+            } else {
+                answer.add(String.valueOf(i));
+            }
+        }
+
+        return answer;
+    }
+}
