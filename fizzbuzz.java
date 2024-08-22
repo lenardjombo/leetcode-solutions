509e382c677b9324e75359e4eaac1f6a7885376f
Leonard Jombo <158037209+lenardjombo@users.noreply.github.com>
2024-08-22 02:22:07 -0400

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,22 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
-
-class Solution {
-    public List<String> fizzBuzz(int n) {
+class FizzBuzz {
+    //method
+    public List<String> fizzBuzz(int n){
         List<String> answer = new ArrayList<>();
-
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+        for(int i = 1;i <= n;i++){
+            if(i % 3 == 0 && i % 5 == 0){
                 answer.add("FizzBuzz");
-            } else if (i % 3 == 0) {
+            }else if(i % 3 == 0){
                 answer.add("Fizz");
-            } else if (i % 5 == 0) {
+            }else if(i % 5 == 0){
                 answer.add("Buzz");
-            } else {
+            }else{
                 answer.add(String.valueOf(i));
             }
         }
-
         return answer;
+    }
+    public static void main(String[] args) {
+        FizzBuzz buzz = new FizzBuzz();
+        System.out.println(buzz.fizzBuzz(15));
     }
 }
