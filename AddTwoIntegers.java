b2030da5028f761966d711beb57fb5ee393d7472
Leonard Jombo <158037209+lenardjombo@users.noreply.github.com>
2024-08-22 06:01:20 -0400

    Create AddTwoIntegers.java

---
+++ AddTwoIntegers.java
@@ -0,0 +1,21 @@
+# Intuition
+<!-- Describe your first thoughts on how to solve this problem. -->
+
+# Approach
+<!-- Describe your approach to solving the problem. -->
+
+# Complexity
+- Time complexity:
+<!-- Add your time complexity here  -->
+
+- Space complexity:
+<!-- Add your space complexity here -->
+
+# Code
+```java []
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2 ;
+    }
+}
+```
